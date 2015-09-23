eb8cefcf7b07551442a7227542a556b0840be834
Samuel Colvin <s@muelcolvin.com>
2015-09-23 19:12:34 +0100

    release to pypi

--- setup.py
+++ setup.py
@@ -1,13 +1,18 @@
+import sys
 from setuptools import setup
 
+long_description = ''
+if 'upload' in sys.argv or 'register' in sys.argv:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
 
 setup(
-    name='django-bootstrap3-datetimepicker',
+    name='django-bootstrap3-datetimepicker-2',
     packages=['bootstrap3_datetime'],
     include_package_data=True,
-    version='2.4',
+    version='2.4.2',
     description='Bootstrap3 compatible datetimepicker for Django projects.',
-    long_description=open('README.md').read(),
+    long_description=long_description,
     author='Nakahara Kunihiko/Samuel Colvin',
     author_email='nakahara.kunihiko@gmail.com/s@muelcolvin.com',
     url='https://github.com/samuelcolvin/django-bootstrap3-datetimepicker',
