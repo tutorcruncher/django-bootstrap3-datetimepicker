839b2776454b53d2c4af3e2497e853a3e66c0df6
tomhamiltonstubber <tomhamiltonstubber@gmail.com>
2018-05-09 11:46:46 +0100

    Add travis and uprev

--- setup.py
+++ setup.py
@@ -6,15 +6,17 @@ if 'upload' in sys.argv or 'register' in sys.argv:
     import pypandoc
     long_description = pypandoc.convert('README.md', 'rst')
 
+VERSION = '2.6.0'
+
 setup(
     name='django-bootstrap3-datetimepicker-2',
     packages=['bootstrap3_datetime'],
     include_package_data=True,
-    version='2.5.0',
+    version=VERSION,
     description='Bootstrap3 compatible datetimepicker for Django projects.',
     long_description=long_description,
     author='Nakahara Kunihiko/Samuel Colvin',
-    author_email='nakahara.kunihiko@gmail.com/s@muelcolvin.com',
+    author_email='s@muelcolvin.com',
     url='https://github.com/samuelcolvin/django-bootstrap3-datetimepicker',
     license='Apache License 2.0',
     classifiers=[
